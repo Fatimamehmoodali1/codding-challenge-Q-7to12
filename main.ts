904299fd7166524907648ac01622a58646115820
Fatimamehmoodali1 <162306190+Fatimamehmoodali1@users.noreply.github.com>
2024-03-07 02:15:03 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,34 @@
+//Q7,Q8:
+//console.log(4+4);
+//console.log(10-2);
+//console.log(2*4);
+//console.log(16/2);
+//Q9:
+//let favouriteNumber=1;
+//console.log("My favourite number is", favouriteNumber);
+//Q10:
+//date: 4-march-2024
+//let whiteSpace = "\n\t Fatima mehmood ali\t\n";
+//console.log(whiteSpace);
+//let withoutwhiteSpace = whiteSpace.trim();
+//console.log(withoutwhiteSpace);
+
+//Q11:
+//let friendName =["Fatima","Zainab","Wajeeha","shabaz","shayan","Hunain","Zaryab","Muskan","Musab"]
+//console.log(friendName[0])
+//console.log(friendName[1])
+//console.log(friendName[2])
+//console.log(friendName[3])
+//console.log(friendName[4])
+//console.log(friendName[5])
+//console.log(friendName[6])
+//console.log(friendName[7])
+//console.log(friendName[8])
+//Q12
+let friendName =["Fatima","zainab","wajeeha","shabaz"]
+let message="hello how are you";
+console.log("HELLO",friendName[0],message)
+console.log("HELLO",friendName[1],message)
+console.log("HELLO",friendName[2],message)
+console.log("HELLO",friendName[3],message)
+
